835251b7569035e9fc78066a6c80f2855ee2f119
it1shka <2tishbel@gmail.com>
2024-06-23 18:13:15 +0200

    Started working on navigation

--- app/controller.py
+++ app/controller.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+import tkinter as tk
 from core import Client
 from .interface import Interface
 
@@ -19,5 +20,30 @@ class Controller:
 
     def start(self) -> None:
         '''Starts the application'''
-        ...
+        self._bind_iface()
         self._iface.window.mainloop()
+
+    def _bind_iface(self) -> None:
+        '''Attaches listeners to UI elements'''
+        self._iface.connect_button.config(command=self._handle_connect)
+        # TODO: add rest
+        
+    def _handle_connect(self) -> None:
+        '''Connects to new MongoDB client'''
+        try:
+            dest = self._iface.url_text.get('1.0', tk.END + '-1c')
+            self._client.connect(dest)
+        except Exception:
+            self._client.disconnect()
+        finally:
+            self._update_navigation()
+
+    def _update_navigation(self) -> None:
+        '''Reads all databases and collections'''
+        tree = self._iface.navigation_treeview
+        for row in tree.get_children():
+            tree.delete(row)
+        for db_name in self._client.databases:
+            db_root = tree.insert('', tk.END, text=db_name)
+            for collection_name in self._client.collections(db_name):
+                collection_root = tree.insert(db_root, tk.END, text=collection_name)
--- app/interface.py
+++ app/interface.py
@@ -10,13 +10,16 @@ class Interface:
     '''
 
     window: tk.Tk
-    url_input: tk.Text
+    url_text: tk.Text
     connect_button: ttk.Button
     open_browser_button: ttk.Button
+    navigation_treeview: ttk.Treeview
 
     def __init__(self) -> None:
         self._build_window()
-        self._build_url_input()
+        self._build_url_text()
+        self._build_navigation()
+        self._build_explorer()
 
     def _build_window(self) -> None:
         '''Creates Tk window'''
@@ -24,18 +27,40 @@ class Interface:
         self.window.title('Mango')
         self.window.attributes('-fullscreen', True)
 
-    def _build_url_input(self) -> None:
+    def _build_url_text(self) -> None:
         '''
         Adds widget that allows you to connect
         to particular MongoDB instance
         '''
         widget = ttk.Frame(self.window)
         label = ttk.Label(widget, text='Connect to: ')
-        self.url_input = tk.Text(widget, height=1)
+        self.url_text = tk.Text(widget, height=1, width=50)
         self.connect_button = ttk.Button(widget, text='Connect')
         self.open_browser_button = ttk.Button(widget, text='Open in browser')
-        children = [label, self.url_input, self.connect_button, self.open_browser_button]
+        children = [label, self.url_text, self.connect_button, self.open_browser_button]
         for each in children:
             each.pack(side=tk.LEFT)
         widget.config(padding=5, borderwidth=1, relief='solid')
         widget.pack(side=tk.TOP, fill='x')
+    
+    def _build_navigation(self) -> None:
+        '''Adds navigation Treeview on the left side'''
+        widget = ttk.Frame(self.window)
+        widget.config(borderwidth=1, relief='solid')
+        widget.pack(side=tk.LEFT, fill='y')
+        self.navigation_treeview = ttk.Treeview(widget, show='tree')
+        scrollbar = ttk.Scrollbar(
+            widget,
+            orient=tk.VERTICAL,
+            command=self.navigation_treeview.yview,
+        )
+        self.navigation_treeview.config(yscrollcommand=scrollbar.set)
+        scrollbar.pack(side=tk.RIGHT, fill='y')
+        self.navigation_treeview.pack(fill='both', expand=True)
+
+    def _build_explorer(self) -> None:
+        '''Builds main view to edit documents'''
+        widget = ttk.Frame(self.window)
+        widget.config(borderwidth=1, relief='solid')
+        # TODO:
+        widget.pack(side=tk.RIGHT, fill='both', expand=True)
