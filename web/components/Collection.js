d2b0a287adb8f26803b82422b4ee0a1d7f06c70d
it1shka <2tishbel@gmail.com>
2024-07-01 00:19:08 +0200

    I think i finished the navigation

--- web/components/Collection.js
+++ web/components/Collection.js
@@ -1,15 +1,34 @@
+import { computed } from 'vue'
+import store, { setChosen } from '../store.js'
+
 export default {
   template: `
-    <div :class="{ chosen: isChosen, collection: true }">
+    <div 
+      @click="choose"
+      :class="{ chosen: isChosen, collection: true }"
+    >
       <p>{{ name }}</p>
     </div>
   `,
   props: ['database', 'name'],
   setup({ database, name }) {
-    
+    const choose = () => {
+      setChosen({
+        database,
+        collection: name,
+      })
+    }
+
+    const isChosen = computed(() => {
+      if (!store.chosen) return false
+      return store.chosen.database === database &&
+        store.chosen.collection === name
+    })
+
     return {
       name,
-      isChosen: false
+      isChosen,
+      choose,
     }
   },
 }
