05f58ab7f2307a06d433bcb205550759cc4903f1
it1shka <2tishbel@gmail.com>
2024-06-30 14:17:33 +0200

    Created layout for header

--- web/components/Explorer.js
+++ web/components/Explorer.js
@@ -1,7 +1,7 @@
 export default {
   template: `
     <div id="explorer">
-
+      
     </div>
   `
 }
--- web/components/Header.js
+++ web/components/Header.js
@@ -1,7 +1,12 @@
 export default {
   template: `
     <header id="header">
-
+      <h2>Connect to: </h2>
+      <input 
+        type="text"
+        placeholder="Connection string"
+      />
+      <button>Connect</button>
     </header>
   `
 }
