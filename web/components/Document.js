260ccd97c688c877ad8204edc14c2d3bbee235a9
it1shka <2tishbel@gmail.com>
2024-07-03 17:25:48 +0200

    Created panel for document edit

--- web/components/Document.js
+++ web/components/Document.js
@@ -1,28 +1,84 @@
-import { ref, toRef } from 'vue'
-
-// TODO: 
+import { ref, toRef, watch, computed } from 'vue'
+import {identifyDocument} from '../lib.js'
 
 export default {
   template: `
     <div 
+      @click="open"
       class="collection-element"
     >
       <img alt="Document" src="/images/document.png" />
-      <p>Document</p>
+      <p>{{ title }}</p>
     </div>
+    <Teleport to="body">
+      <aside 
+        v-if="isOpen" 
+        @click.stop="close"
+        class="fullscreen-panel"
+      >
+        <form 
+          @submit.prevent="submit"
+          @click.stop
+          class="inner-container"
+        >
+          <h1 class="label">Edit document:</h1>
+          <textarea ref="editorRef">{{ json }}</textarea>
+          <!-- TODO: add delete button -->
+          <button type="submit">Edit</button>
+        </form>
+      </aside>
+    </Teleport>
   `,
   props: ['json'],
   setup(props) {
-    const json = toRef(props, 'json')
+    const jsonWithId = toRef(props, 'json')
+    const json = computed(() => {
+      const initial = JSON.parse( JSON.stringify(jsonWithId.value))
+      delete initial['_id']
+      return initial
+    })
+
+    const title = computed(() => {
+      return identifyDocument(json.value)
+    })
 
     const isOpen = ref(false)
     const open = () => { isOpen.value = true }
     const close = () => { isOpen.value = false }
 
+    const editorRef = ref(null)
+    const editorValue = ref('')
+    watch([isOpen, editorRef], () => {
+      if (!editorRef.value || !isOpen) return
+      editorValue.value = json.value
+      const cm = CodeMirror.fromTextArea(editorRef.value, {
+        lineNumbers: true,
+        mode: 'javascript',
+        json: true,
+      })
+      cm.setSize(450, 450)
+      cm.on('change', () => {
+        editorValue.value = cm.getValue()
+      })
+    })
+
+    // TODO: 
+    const submit = () => {
+      try {
+
+      } catch {
+
+      }
+    }
+
     return {
+      json, 
+      title,
       isOpen,
+      editorRef,
       open,
       close,
+      submit,
     }
   },
 }
--- web/lib.js
+++ web/lib.js
@@ -1,3 +1,31 @@
 export const apiURL = relative => {
   return `http://localhost:3131/api/${relative}`
 }
+
+const nestedEntries = obj => {
+  if (typeof obj !== 'object') return []
+  const entries = Object.entries(obj)
+  const output = []
+  for (const [key, value] of entries) {
+    if (typeof value !== 'object') {
+      output.push([key, value])
+    } else {
+      output.push(...nestedEntries(value))
+    }
+  }
+  return output
+}
+
+
+export const identifyDocument = doc => {
+  const targets = ['name', 'tag', 'label', 'title', 'id']
+  const entries = nestedEntries(doc)
+  for (const target of targets) {
+    for (const [key, value] of entries) {
+      if (key.includes(target)) {
+        return value
+      }
+    }
+  }
+  return 'Document'
+}
